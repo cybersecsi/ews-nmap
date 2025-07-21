a95a8181f16481befc03f06770b18d5e2120c034
emalderson <emalderson@users.noreply.github.com>
2025-07-21 13:20:02 +0200

    feat/fix: add support to csv generation and remove default ports from baseurls

--- ewsnmap/ewsnmap.py
+++ ewsnmap/ewsnmap.py
@@ -36,11 +36,12 @@ def check_or_create_dir(dir_path):
         makedirs(dir_path)
         info(f"Directory {dir_path} created")
 
-def process(nmap_xml_file,  output_dir = OUTPUT_DIR, output = 'output.txt', append=False):
+def process(nmap_xml_file,  output_dir = OUTPUT_DIR, txtout = 'output.txt', csvout = 'output.csv', append=False):
     """
     Extract web servers from an Nmap XML file
     """
     targets = []
+    services = []
 
     info(f"Opening XML file {nmap_xml_file}")
     nmap_report = NmapParser.parse_fromfile(nmap_xml_file)
@@ -48,27 +49,45 @@ def process(nmap_xml_file,  output_dir = OUTPUT_DIR, output = 'output.txt', appe
         #if host.is_up():
         for service in host.services:
             if service.state == 'open':
-                if 'https' in service.service:
-                    targets.append("https://%s:%s" % (host.address, service.port))
-                    info("https://%s:%s" % (host.address, service.port))
+                # Create the version string
+                version = ""
+                service_product = service.service_dict.get("product",None)
+                service_version = service.service_dict.get("version",None)
+                if service_product:
+                    version = service_product
+                if service_version:
+                    version = version + " " + service_version
 
-                elif 'http' in service.service:
-                    targets.append("http://%s:%s" % (host.address, service.port))
-                    info("http://%s:%s" % (host.address, service.port))
-
-                if 'HTTP/' in service.servicefp:
-                    targets.append("http://%s:%s" % (host.address, service.port))
-                    info("http://%s:%s" % (host.address, service.port))
+                if 'http' in service.service or 'HTTP/' in service.servicefp:
+                    service_row = f"{host.address},{service.port},{service.protocol},{service.service},{version}"
+                    services.append(service_row)
+                    info(service_row)
+                    scheme = 'https' if 'https' in service.service else 'http'
+                    if service.port in [80, 443]:
+                        target = f"{scheme}://{host.address}"
+                    else:
+                        target = f"{scheme}://{host.address}:{service.port}"
+                    targets.append(target)
+                    info(target)
 
     check_or_create_dir(output_dir)
 
     file_mode = 'a+' if append else 'w'
 
-    with open(path.join(output_dir, output), file_mode, encoding='utf-8', newline='') as file:
-        info(f"Store evidences in {path.join(output_dir, output)}")
+    with open(path.join(output_dir, txtout), file_mode, encoding='utf-8', newline='') as file:
+        info(f"Store baseurls in {path.join(output_dir, txtout)}")
         for target in targets:
             file.write(f"{target}\n")
-        info("File correctly created!")
+        info("TXT file correctly created!")
+
+    with open(path.join(output_dir, csvout), file_mode, encoding='utf-8', newline='') as file:
+        info(f"Store services in {path.join(output_dir, csvout)}")
+        if not append:
+            header = "ip,port,protocol,service,version"
+            file.write(f"{header}\n")
+        for service in services:
+            file.write(f"{service}\n")
+        info("CSV file correctly created!")
 
 def main():
     try:
